ee3253834ef3de359d3a37dce5f4e8a3eae7d4c5
Marco Pérez <mperez@unitedvirtualities.com>
2020-08-30 18:22:48 -0600

    Task_1: convert the base templates to PHP files

---
+++ src/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Marco Antonio Pérez Rivera</title>
+
+    <link href='https://fonts.googleapis.com/css?family=Raleway:400,300,600' rel='stylesheet' type='text/css'>
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/skeleton.css">
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    <header>
+        <div class="container">
+            <nav class="navbar">
+                <ul class="navbar-main">
+                    <li class="navbar-main-item navbar-main-item-logo"><a href="#">Marco Pérez</a></li>
+                    <li class="navbar-main-item"><a href="#contact">Contacto</a></li>
+                    <li class="navbar-main-item"><a href="#skills">Destrezas</a></li>
+                    <li class="navbar-main-item"><a href="#bio">Bio</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
